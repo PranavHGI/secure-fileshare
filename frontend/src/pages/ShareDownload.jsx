ece1cbb4310853b84eed394adf06ba37f25c2031
Pranav Agalgave <pranavagalgave4011@gmail.com>
2026-07-05 09:40:48 -0400

    link updated

--- frontend/src/pages/ShareDownload.jsx
+++ frontend/src/pages/ShareDownload.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
-
-const api = axios.create({ baseURL: 'http://localhost:3000/api' });
+import api from '../api/axios';
 
 export default function ShareDownload() {
   const { token } = useParams();
